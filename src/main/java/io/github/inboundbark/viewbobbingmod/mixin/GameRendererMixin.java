5af183f0f2474b79d58ed521a091d1d0504b3eca
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2021-12-03 11:05:28 +0100

    simplify mixin (those operations were NOPs)

--- src/main/java/io/github/inboundbark/viewbobbingmod/mixin/GameRendererMixin.java
+++ src/main/java/io/github/inboundbark/viewbobbingmod/mixin/GameRendererMixin.java
@@ -1,28 +1,15 @@
 package io.github.inboundbark.viewbobbingmod.mixin;
 
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.util.math.Vec3f;
-import net.minecraft.entity.player.PlayerEntity;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(GameRenderer.class)
 public abstract class GameRendererMixin {
-
-
-    @Shadow @Final private MinecraftClient client;
-
     @Redirect(method = "renderWorld", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/GameRenderer;bobView(Lnet/minecraft/client/util/math/MatrixStack;F)V"))
-    private void bobView(GameRenderer gameRenderer, MatrixStack matrixStack, float f) {
-        if (this.client.getCameraEntity() instanceof PlayerEntity) {
-            matrixStack.translate((0.0F), (0.0F), 0.0D);
-            matrixStack.multiply(Vec3f.POSITIVE_Z.getDegreesQuaternion(0.0F));
-            matrixStack.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion(0.0F));
-        }
+    private void dontBobView(GameRenderer gameRenderer, MatrixStack matrixStack, float tickDelta) {
+        // do nothing
     }
 }
